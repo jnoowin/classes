195d301589eecfa480d38464cca707ddaba9f26e
Jared Nguyen <jtn2km@virginia.edu>
2022-01-20 22:27:48 -0500

    Tailwindcss (#1)
    * Added TailwindCSS
    
    * Installed heroicons and headlessui
    Added custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "space-cadet": "#2b2d42",
+        "light-slate": "#7c90a0",
+        bone: "#e6dfd5",
+        rajah: "#ffad69",
+        "dark-orange": "#f58f29",
+      },
+    },
+  },
+  plugins: [],
+};
