e0f047e1a178ca075e82452758c82a4b150b3793
blarghnog <jtn2km@virginia.edu>
2022-02-23 12:14:25 -0500

    Adjusted layout component to take mult children

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import Navbar from "./navbar";
 
 interface LayoutProps {
-  children: React.ReactChild;
+  children: React.ReactChild[];
 }
 
 function Layout({ children }: LayoutProps) {
   return (
     <>
       <Navbar />
-      {React.Children.map(children, (component, index) => component)}
+      <div className="flex flex-col items-center bg-neutral-100 pt-10">
+        {React.Children.map(children, (component) => component)}
+      </div>
     </>
   );
 }
